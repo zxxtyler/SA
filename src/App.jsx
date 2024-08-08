ecf0afbcd7a1a3c31c425e3cffe992481e8f879e
ELOAH ROSSI PIRES <eloah_pires@estudante.sesisenai.org.br>
2024-08-08 09:57:05 -0300

    login

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import imagem1 from './img/logo.svg'
+import imagem2 from './img/idoso.svg'
+import './App.css'
+import React from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header> 
+      <img src={imagem1}/>
+        <a href="#">Home</a>
+        <a href="#">Como funciona?</a>
+        <a href="#">Benefícios</a>
+        <a href="#">Sobre nós</a>
+        <p>Ainda não tem uma conta? <a href="//">Clique aqui</a></p>
+      </header>
+
+      <div className='container'>
+        <img className='velho' src={imagem2}/> 
+        <div className='login'>
+        <h1>Login</h1>
+        <p className='introducao'>Bom te ver novamente! Preencha as informações para entrar em sua conta!</p>
+        <input type="text" placeholder='nome'/> 
+        <br />
+        <input type="text" placeholder='email'/>
+        <br />
+        <input type="text" placeholder='senha'/>
+        <br />
+        <center>
+        <button className='logar'>Logar</button>
+        </center> 
+        <br />
+        <center>
+        <p>Ou entre por outro meio:</p>
+        </center>      
+      </div>
+      </div>
+      
+
+      
+      
+     
+    </>
+  )
+}
+
+export default App
