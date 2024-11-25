dd6f3ee93caa301c4d18f84a8781aa6247ecd886
iaramaia09 <iara.maiads09@gmail.com>
2024-11-24 21:26:07 -0300

    aaall

--- front/src/cadastro.jsx
+++ front/src/cadastro.jsx
@@ -1,59 +1,55 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import imagem1 from '../public/img/logo.png';
 import imagem3 from '../public/img/velho2.png';
 import imagem4 from '../public/icons/fotoperfil.svg';
 import './css/cadastrologin.css';
-import login from './login';
-import { Link } from 'react-router-dom';
 
 function Cadastro() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('');
+  const [phone, setPhone] = useState('');
+  const [bio, setBio] = useState('');
+  const navigate = useNavigate(); // Para redirecionar após cadastro
+
+  const handleCadastro = () => {
+    const userData = { username, dateOfBirth, phone, bio };
+    // Salva o usuário no localStorage
+    localStorage.setItem('user', JSON.stringify(userData));
+    navigate('/login'); // Redireciona para a página de login
+  };
 
   return (
     <>
-
-<header> 
-      <img className='logo' src={imagem1}/>
+      <header> 
+        <img className='logo' src={imagem1}/>
         <p><Link to="/">Home</Link></p>
         <a href="#">Como funciona?</a>
         <a href="#">Benefícios</a>
         <a href="#">Sobre nós</a>
-        <p className='separado'> Já tem uma conta? <Link to="/login" >Clique aqui</Link></p>
+        <p className='separado'> Já tem uma conta? <Link to="/login">Clique aqui</Link></p>
       </header>
 
-       
       <div className='container'>
         <img className='velho' src={imagem3}/> 
         <div className='login'>
-        <h1 className='cadastr'>Cadastro</h1>
-        <p className='introducao'>Bom te ver! Preencha as informações para se cadastrar em sua conta!</p>
-        <input className='inputmedio' type="text" placeholder='nome de usuário'/> 
-        <br />
-
-
-        <div className='doisinputs'>
-        <img className='fotoperfil' src={imagem4} />
-        <div className='coluna'>
-        <input className='inputpequeno' type="date" placeholder='data de nascimento'/>
-        <br />
-        <input className='inputpequeno' type="text" placeholder='telefone'/>
+          <h1 className='cadastr'>Cadastro</h1>
+          <input className='inputmedio' type="text" placeholder='nome de usuário' value={username} onChange={(e) => setUsername(e.target.value)} />
+          <div className='doisinputs'>
+            <img className='fotoperfil' src={imagem4} />
+            <div className='coluna'>
+              <input className='inputpequeno' type="date" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} />
+              <input className='inputpequeno' type="text" placeholder='telefone' value={phone} onChange={(e) => setPhone(e.target.value)} />
+            </div>
+          </div>
+          <textarea className='inputmaior' placeholder='Biografia' value={bio} onChange={(e) => setBio(e.target.value)} />
+          <center>
+            <button className='logar' onClick={handleCadastro}>Cadastrar</button>
+          </center> 
         </div>
-        </div>
-
-        <br />
-        <textarea className='inputmaior' placeholder='Biografia'></textarea>
-        <br />
-        <center>
-        <button className='logar'><Link className='logarbotao' to="/home">Cadastrar</Link></button>
-        </center> 
-           
       </div>
-      </div>
-      
-   
     </>
   )
 }
 
-export default Cadastro
+export default Cadastro;
--- front/src/login.jsx
+++ front/src/login.jsx
@@ -1,54 +1,56 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import imagem1 from '../public/img/logo.png';
 import imagem2 from '../public/img/idoso.png';
 import imagem3 from '../public/icons/google.svg';
 import './css/cadastrologin.css';
-import Cadastro from './cadastro';
-import { Link } from 'react-router-dom';
-import './css/login.css';
-
 
 function Login() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState('');
+  const [error, setError] = useState(false);
+  const navigate = useNavigate(); // Para redirecionar após login
+
+  const handleLogin = () => {
+    const storedUser = JSON.parse(localStorage.getItem('user'));
+    if (storedUser && storedUser.username === username) {
+      // Usuário existe, redireciona para a home
+      navigate('/home');
+    } else {
+      // Mostra erro
+      setError(true);
+    }
+  };
 
   return (
     <>
       <header> 
-      <img className='logo' src={imagem1}/>
-       <p><Link to="/">Home</Link></p>
+        <img className='logo' src={imagem1}/>
+        <p><Link to="/">Home</Link></p>
         <a href="#">Como funciona?</a>
         <a href="#">Benefícios</a>
         <a href="#">Sobre nós</a>
-        <p className='separado'> Ainda não tem uma conta? <Link to="/cadastro" >Clique aqui</Link></p>
+        <p className='separado'> Ainda não tem uma conta? <Link to="/cadastro">Clique aqui</Link></p>
       </header>
 
       <div className='container'>
         <img className='velho' src={imagem2}/> 
         <div className='login'>
-        <h1>Login</h1>
-        <p className='introducao'>Bom te ver novamente! Preencha as informações para entrar em sua conta!</p>
-        <input type="text" placeholder='nome de usuário'/> 
-        <br />
-        <input type="text" placeholder='senha'/>
-        <br />
-        <center>
-        <button className='logar'><Link className='logarbotao' to="/home">Logar</Link></button>
-        </center> 
-        <br />
-        <center>
-        <p>Ou entre por outro meio:</p>
-        <img className='google' src={imagem3}/>
-        </center>      
+          <h1>Login</h1>
+          <input 
+            type="text" 
+            placeholder='nome de usuário' 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} 
+            className={error ? 'input-error' : ''}
+          />
+          {error && <p className='error-message'>Usuário não encontrado!</p>}
+          <center>
+            <button className='logar' onClick={handleLogin}>Logar</button>
+          </center> 
+        </div>
       </div>
-      </div>
-      
-
-      
-      
-     
     </>
   )
 }
 
-export default Login
+export default Login;
